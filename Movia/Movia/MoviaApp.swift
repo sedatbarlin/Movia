810ee9fddfb120f3e8da514afeac50695c577b31
Sedat Barlin <barlinsedat@gmail.com>
2025-05-22 13:59:53 +0300

    Initial Commit with Tests

---
+++ Movia/Movia/MoviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviaApp.swift
+//  Movia
+//
+//  Created by Sedat on 22.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct MoviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
